052d625ec010cc95bc01b6e269581ddeed587a68
yipan-easybling <yipan@easybilling.cloud>
2026-03-31 11:23:02 +0800

    dsds

---
+++ eb-test/src/main/java/com/eb/webhook/EbTestApplication.java
@@ -0,0 +1,11 @@
+package com.eb.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EbTestApplication.class, args);
+    }
+}
