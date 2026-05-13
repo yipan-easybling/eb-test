a656cade1f29b740ab00a571b7537aae2e9308ae
yipan-easybling <yipan@easybilling.cloud>
2026-05-13 16:00:51 +0800

    Add WebhookUser entity and update workspace configuration

--- eb-test/src/main/java/com/eb/webhook/TestController.java
+++ eb-test/src/main/java/com/eb/webhook/TestController.java
@@ -1,5 +1,6 @@
 package com.eb.webhook;
 
+import com.eb.webhook.entity.WebhookUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,8 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        WebhookUser webhookUser = new WebhookUser();
+        webhookUser.setEmail("110@163.com");
         return "test -----v2.0.0";
     }
 }
