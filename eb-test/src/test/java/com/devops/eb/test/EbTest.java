cdb5e902f724ed126d4c1e749f5013db78cc1963
yipan-easybling <yipan@easybilling.cloud>
2026-05-14 15:00:05 +0800

    Add WebhookUser entity and update workspace configuration

---
+++ eb-test/src/test/java/com/devops/eb/test/EbTest.java
@@ -0,0 +1,11 @@
+package com.devops.eb.test;
+
+import org.junit.jupiter.api.Test;
+
+public class EbTest {
+
+    @Test
+    public void test() {
+        System.out.println("test -----v2.0.0");
+    }
+}
